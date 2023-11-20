9f1b79d2db2cef82dc2da85a9a5d3e93932d8840
alegohar <102521210+alegohar@users.noreply.github.com>
2023-11-20 22:21:42 +0500

    Update Path.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import Homepage from 'src/Pages/Homepage';
-import About from "src/Pages/About";
-import Contact from "src/Pages/Contactus";
-import Careers from "src/Pages/Careers";
-import CustForm from "src/Pages/form-sweetalert"
-import StopWatch from "src/pages/StopWatch";
-import PostsItem from "src/pages/posts";
-import Task from "src/pages/tasks";
+import { HomePage } from "@Pages/Homepage";
+import About from "@Pages/About";
+import Contact from "@Pages/Contactus";
+import Careers from "@Pages/Careers";
+import CustForm from "@Pages/form-sweetalert"
+import StopWatch from "@Pages/StopWatch";
+import PostsItem from "@Pages/posts";
+import Task from "@Pages/tasks";
+
 const defaultOptions = {
   loop: true,
   autoplay: true,
