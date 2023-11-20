6fcd91ccf6aced073e6e448494d687566aa9b93d
alegohar <102521210+alegohar@users.noreply.github.com>
2023-11-20 22:22:22 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      '@Pages': Path.resolve(__dirname, '../Pages')
+    }
+  },
   plugins: [react()],
 })
